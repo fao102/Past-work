520af5de5c1de04447ba4f5ae71974f21b0ebed8
Filé <fiajan1602@gmail.com>
2023-01-20 15:33:46 +0000

    best performance so far

--- NeuralNet.py
+++ NeuralNet.py
@@ -5,6 +5,59 @@ from torch.autograd import Variable
 from matplotlib.colors import ListedColormap
 import numpy
 from datetime import datetime
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, classification_report
+
+
+def plot_decision_boundary(func, X_tensor, y_tensor, figsize=(9, 6)):
+    X = X_tensor.to("cpu").detach().numpy()
+    y = y_tensor.to("cpu").detach().numpy()
+    func.to("cpu")
+    amin, bmin = X.min(axis=0) - 0.1
+    amax, bmax = X.max(axis=0) + 0.1
+    hticks = np.linspace(amin, amax, 101)
+    vticks = np.linspace(bmin, bmax, 101)
+    
+    aa, bb = np.meshgrid(hticks, vticks)
+    ab = Variable(torch.from_numpy(np.c_[aa.ravel(), bb.ravel()]).float())
+    c = func.pred(ab).detach().numpy()
+    cc = c.reshape(aa.shape)
+
+    cm = plt.cm.RdBu
+    cm_bright = ListedColormap(['#FF0000', '#0000FF'])
+    
+    fig, ax = plt.subplots(figsize=figsize)
+    contour = plt.contourf(aa, bb, cc, cmap=cm, alpha=0.8)
+    
+    ax_c = fig.colorbar(contour)
+    ax_c.set_label("$P(y = 1)$")
+    ax_c.set_ticks([0, 0.25, 0.5, 0.75, 1])
+    
+    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=cm_bright)
+    plt.xlim(amin, amax)
+    plt.ylim(bmin, bmax)
+
+def plot_confusion_matrix(y, y_pred):
+    plt.figure(figsize=(8, 6))
+    sns.heatmap(pd.DataFrame(confusion_matrix(y, y_pred)), annot=True, fmt='d', cmap='YlGnBu', alpha=0.8, vmin=0)
+
+
+
+def twospirals(n_points, noise=0):
+    """
+     Returns the two spirals dataset.
+     https://glowingpython.blogspot.com/2017/04/solving-two-spirals-problem-with-keras.html
+    """
+    n = np.sqrt(np.random.rand(n_points,1)) * 780 * (2*np.pi)/360
+    d1x = -np.cos(n)*n + np.random.rand(n_points,1) * noise
+    d1y = np.sin(n)*n + np.random.rand(n_points,1) * noise
+    return (np.vstack((np.hstack((d1x,d1y)),np.hstack((-d1x,-d1y)))), 
+            np.hstack((np.zeros(n_points),np.ones(n_points))))
+
 
 def recommand_tanh(x):
   return 1.7159*torch.tanh((2/3)*x)
@@ -12,13 +65,13 @@ def recommand_tanh(x):
 
 class FileNN(nn.Module):#inherits from nn.module
     def __init__(self) -> None:
-        super().__init__(self) 
+        super().__init__() 
         self.layer_1_net = nn.Linear(2,800,bias=True) 
         self.layer_1_act = recommand_tanh
-        self.layer_1_1_activation = recommand_tanh
+        self.layer_1_1_act = recommand_tanh
         self.layer_1_1_net = nn.Linear(800,800,bias=True)
         self.layer_2_act = nn.Sigmoid()
-        self.layer_2_net = nn.Linear(800,800,bias=True)#f(linear)=Wx +b
+        self.layer_2_net = nn.Linear(800,1,bias=True)#f(linear)=Wx +b
 
     def train(self, data, num_epochs = 10, lr = 0.003):
         criterion = nn.BCELoss()#loss function
@@ -27,7 +80,7 @@ class FileNN(nn.Module):#inherits from nn.module
             for item in data:
                 out = self(item[0])
                 optimizer.zero_grad()
-                loss = criterion(torch.squeeze(out, item[1]))
+                loss = criterion(torch.squeeze(out), item[1])
                 loss.backward()
                 optimizer.step()
 
@@ -61,16 +114,40 @@ class FileNN(nn.Module):#inherits from nn.module
         return torch.tensor(ans)
 
 
+X, y = twospirals(500)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5)
 
-learning_rate = 0.003
+learning_rate = 0.1
 batch_size = 32
-num_epochs = 500
+num_epochs = 850
 
-dataset = TensorDataset(x_train, y_train)
+
+
+x_train_tensor = torch.tensor(X_train, dtype=torch.float, device="cpu")
+y_train_tensor = torch.tensor(y_train, dtype=torch.float, device="cpu")
+dataset = TensorDataset(x_train_tensor, y_train_tensor)
 loader = DataLoader(dataset, batch_size = batch_size)
+
 
 FileNet = FileNN()
 FileNet.to("cpu")
 strat_time = datetime.now()
 FileNet.train(loader,num_epochs=num_epochs, lr=learning_rate)
 end_time = datetime.now()
+
+print('time spend in the training: ', end_time - strat_time)
+plot_decision_boundary(FileNet, x_train_tensor, y_train_tensor)
+
+
+x_test_tensor = torch.tensor(X_test, dtype=torch.float)
+y_test_tensor = torch.tensor(y_test, dtype=torch.long)
+
+test_pred = FileNet.pred(x_test_tensor)
+test_pred_np = test_pred.detach().numpy()
+
+num_correct = np.sum(y_test == test_pred_np)
+print("accuracy: ", num_correct/len(y_test))
+
+classification_results = classification_report(y_test, test_pred_np)
+print(classification_results)
+plot_confusion_matrix(y_test, test_pred_np)
